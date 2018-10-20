26ab4a4d1361b956a16b75ac4f608a92012bb512
RafalWKot <rafal.kot.poczta@gmail.com>
2018-10-20 17:54:47 +0200

    Start with the new Sudoku application . This is a continue of the  project that began in the kodilla course. Refactor class Load (load texts from file)

---
+++ sudoku/src/main/java/com/rafalwkot/sudoku/Application.java
@@ -0,0 +1,15 @@
+package com.rafalwkot.sudoku;
+
+public class Application {
+
+    public static final String GAMETEXTS = "game_texts.txt";
+
+    public static void main(String[] args) {
+
+        System.out.println(Load.getText(GAMETEXTS, "#GAME_NAME"));
+        System.out.println(Load.getText(GAMETEXTS, "#AUTHOR_NAME") + "\n");
+        System.out.println(Load.getText(GAMETEXTS, "#ABOUT"));
+        System.out.println(Load.getText(GAMETEXTS, "#INTRODUCE") + "\n");
+
+    }
+}
---
+++ sudoku/src/main/java/com/rafalwkot/sudoku/Load.java
@@ -0,0 +1,39 @@
+package com.rafalwkot.sudoku;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Load {
+
+    private static final String EMPTY = "";
+    private static Scanner SCANNER;
+
+    public static String getText(String fileName, String text) {
+        try {
+            SCANNER = new Scanner(getFile(fileName));
+            String textFromFile;
+            while (true) {
+                if (SCANNER.nextLine().equals(text)) {
+                    textFromFile = SCANNER.nextLine();
+                    String line = SCANNER.nextLine();
+                    while (!line.equals(EMPTY)) {
+                        textFromFile = textFromFile + "\n" + line;
+                        line = SCANNER.nextLine();
+                    }
+                    return textFromFile;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Plik z tekstami aplikacji nie został znaleziony " +
+                    "lub niepoprawna jest jego  konfiguracja!");
+        }
+        return "";
+    }
+
+    private static File getFile(String fileName) throws FileNotFoundException {
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        return new File(((Optional.ofNullable(classLoader.getResource(fileName))).orElseThrow(FileNotFoundException::new)).getFile());
+    }
+}
